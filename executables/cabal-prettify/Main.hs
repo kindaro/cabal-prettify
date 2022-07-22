2f5a55b59e15a8032d460dd287728f08a20fcff4
Ignat Insarov <kindaro@gmail.com>
2022-07-22 08:19:00 +0300

    Add an option to merely check.

--- executables/cabal-prettify/Main.hs
+++ executables/cabal-prettify/Main.hs
@@ -2,57 +2,101 @@
 
 module Main where
 
-import Prelude hiding (read, show, error)
-import Prelude qualified
+import Prelude
 import Prelude.Unicode
+import Prelude.Fancy
 
 import Control.Exception
-import Control.Monad
-import Control.Monad.IO.Class
-import Data.ByteString qualified as ByteString
-import Data.ByteString.UTF8 qualified as Utf8
+import Data.ByteString qualified as ByteArray
 import Distribution.Simple.Utils qualified as Cabal
 import Distribution.Verbosity qualified as Cabal
 import Options.Applicative
 import System.FilePath
 import Text.Parsec qualified as Parsec
+import System.IO
+import Data.Maybe
+import System.Exit
+import Control.Monad.Trans.Writer
+import System.Directory
 
 import Distribution.Prettify
 
 instance Exception Parsec.ParseError
 
 main ∷ IO ( )
-main = join do execParser (info (run <**> helper) (fullDesc <> progDesc "Prettify your Cabal package configuration files!"))
+main = do
+  command ← execParser (info (parseCommand <**> helper) (fullDesc <> progDesc "Prettify your Cabal package configuration files!"))
+  run command
 
-run ∷ Parser ( IO ( ))
-run = formatThisPackage <|> formatStandardInput <|> formatArguments
+data Command = Command
+  { targets ∷ Targets
+  , settings ∷ Settings
+  } deriving (Eq, Ord, Show, Read)
 
-formatThisPackage, formatArguments, formatStandardInput ∷ Parser (IO ( ))
+parseCommand ∷ Parser Command
+parseCommand = do
+  targets ← parseTargets
+  settings ← parseSettings
+  pure Command {..}
 
-formatThisPackage = do
-  flag' ( ) (long "this" <> help "Prettify the configuration file of the package you are in right now.")
-  pure do
-    pathToCabalFile ← Cabal.defaultPackageDesc Cabal.normal
-    print pathToCabalFile
-    formatFileArchiving pathToCabalFile
+run ∷ Command → IO ( )
+run Command {..} = do
+  actions ← processTargetsWithSettings targets settings
+  outcomes ← traverse runAction actions
+  if and outcomes then exitSuccess else exitWith (ExitFailure 1)
 
-formatArguments = do
-  targets ← some (argument str (metavar "{cabal files}"))
-  pure do
-    forM_ targets \ target → do
-      formatFileArchiving target
+data Targets = Targets
+  { thisPackage ∷ Bool
+  , standardInput ∷ Bool
+  , arguments ∷ [FilePath]
+  } deriving (Eq, Ord, Show, Read)
 
-formatStandardInput = do
-  flag' ( ) (long "filter" <> help "Prettify standard input.")
-  pure do ByteString.interact (either (Utf8.fromString ∘ Prelude.show) id ∘ format)
+parseTargets ∷ Parser Targets
+parseTargets = do
+  thisPackage ← switch (long "this" <> help "Prettify the configuration file of the package you are in right now.")
+  standardInput ← switch (long "filter" <> help "Prettify standard input.")
+  arguments ← many (argument str (metavar "{cabal files}"))
+  pure Targets {..}
 
-formatFileArchiving ∷ FilePath → IO ( )
-formatFileArchiving target = do
-  contentsOfTarget ← liftIO do ByteString.readFile target
-  case format contentsOfTarget of
+data Settings = Settings
+  { check ∷ Bool
+  } deriving (Eq, Ord, Show, Read)
+
+parseSettings ∷ Parser Settings
+parseSettings = do
+  check ← switch (long "check" <> help "Only check, exit with status 1 when targets need formatting.")
+  pure Settings {..}
+
+data Action = Action
+  { target ∷ Maybe FilePath
+  , check ∷ Bool
+  } deriving (Eq, Ord, Show, Read)
+
+processTargetsWithSettings ∷ Targets -> Settings -> IO [Action]
+processTargetsWithSettings Targets {..} Settings {..} = (fmap catMaybes ∘ sequence ∘ fmap sequence ∘ execWriter) do
+  say do
+    whence thisPackage do
+      pathToCabalFile ← Cabal.defaultPackageDesc Cabal.normal
+      pure Action {target = Just pathToCabalFile, ..}
+  say do whence standardInput (pure Action {target = Nothing, ..})
+  tell do for arguments \ pathToCabalFile → Just (pure Action {target = Just pathToCabalFile, ..})
+
+runAction ∷ Action → IO Bool
+runAction Action {..} = do
+  contents ← bind ByteArray.hGetContents case target of
+    Nothing → pure stdin
+    Just filePath → openFile filePath ReadMode
+  case format contents of
     Left error → throwIO error
-    Right formattedContentsOfTarget → if formattedContentsOfTarget ≡ contentsOfTarget
-      then pure ( )
-      else do
-        ByteString.writeFile (target <.> "previous") contentsOfTarget
-        ByteString.writeFile target formattedContentsOfTarget
+    Right result → if result ≡ contents
+      then pure True
+      else if check
+        then pure False
+        else case target of
+          Nothing → do
+            ByteArray.hPut stdout result
+            pure True
+          Just filePath → do
+            renameFile filePath (filePath <.> "previous")
+            ByteArray.writeFile filePath result
+            pure True
